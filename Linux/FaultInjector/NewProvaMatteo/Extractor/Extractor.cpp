3cb16b4ea405558b8d5e023a998d10456f79272d
MattCamp44 <44680120+MattCamp44@users.noreply.github.com>
2020-11-24 14:06:46 +0100

    Makefile is cool

--- Linux/FaultInjector/NewProvaMatteo/Extractor/Extractor.cpp
+++ Linux/FaultInjector/NewProvaMatteo/Extractor/Extractor.cpp
@@ -1,9 +1,13 @@
 #include "Extractor.h"
+#include<iostream>
 
+using namespace std;
 
 vector<FunctionObject> extractObjects(int pid,char * progname){
 
-
+    vector<FunctionObject> vec;
+    cout << pid << endl;
+    return vec;
 
 
 }
--- Linux/FaultInjector/NewProvaMatteo/main.cpp
+++ Linux/FaultInjector/NewProvaMatteo/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char ** argv){
         return 1;
 
     }
-
+    
     int pid;
 
     if(pid = fork()){
