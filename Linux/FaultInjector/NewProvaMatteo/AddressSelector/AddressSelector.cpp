82b85fbf00621164d9d5942ddc8853ac5cae0298
MattCamp44 <44680120+MattCamp44@users.noreply.github.com>
2020-11-26 13:43:54 +0100

    Breakpoint works

--- Linux/FaultInjector/NewProvaMatteo/AddressSelector/AddressSelector.cpp
+++ Linux/FaultInjector/NewProvaMatteo/AddressSelector/AddressSelector.cpp
@@ -18,7 +18,7 @@ vector<unsigned long int> AddressSelector(vector<FunctionObject> &FunctionObject
     
 
     //This should be somewhat random
-    addresses.emplace_back(FunctionObjects[index].getaddresses()[4]);
+    addresses.emplace_back(FunctionObjects[index].getaddresses()[6]);
     addresses.emplace_back(FunctionObjects[index].getaddresses()[4]);
 
 
